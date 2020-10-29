5cdf734941e8e90799749b6c5872e21e7ff621f0
AnZaiyun <1732478192@qq.com>
2020-10-29 16:36:08 +0800

    配置所有服务进nacos注册中心

--- shoppingmall-product/src/main/java/com/anzaiyun/shoppingmall/product/ShoppingmallProductApplication.java
+++ shoppingmall-product/src/main/java/com/anzaiyun/shoppingmall/product/ShoppingmallProductApplication.java
@@ -3,6 +3,7 @@ package com.anzaiyun.shoppingmall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 整合mybatis-plus
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @MapperScan("com.anzaiyun.shoppingmall.product.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShoppingmallProductApplication {
 
     public static void main(String[] args) {

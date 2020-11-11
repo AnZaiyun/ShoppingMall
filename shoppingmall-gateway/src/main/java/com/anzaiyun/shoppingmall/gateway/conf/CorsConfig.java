1be8d045fd7c41cb3bdbe65288112f0cb43b3fbe
AnZaiyun <1732478192@qq.com>
2020-11-11 09:44:30 +0800

    配置跨域设置

---
+++ shoppingmall-gateway/src/main/java/com/anzaiyun/shoppingmall/gateway/conf/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.anzaiyun.shoppingmall.gateway.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+
+/**
+ * 配置跨域请求头
+ *
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        //是否允许携带cookie信息
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",corsConfiguration);
+
+
+        return new CorsWebFilter(source);
+    }
+}

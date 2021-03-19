f149ab7e39b210b1e448a88568c96498607944d0
AnZaiyun <1732478192@qq.com>
2021-03-19 16:52:40 +0800

    首页跳转

--- shoppingmall-product/src/main/java/com/anzaiyun/shoppingmall/product/controller/web/IndexController.java
+++ shoppingmall-product/src/main/java/com/anzaiyun/shoppingmall/product/controller/web/IndexController.java
@@ -3,12 +3,12 @@ package com.anzaiyun.shoppingmall.product.controller.web;
 import com.anzaiyun.shoppingmall.product.entity.CategoryEntity;
 import com.anzaiyun.shoppingmall.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class IndexController {
 
     @Autowired

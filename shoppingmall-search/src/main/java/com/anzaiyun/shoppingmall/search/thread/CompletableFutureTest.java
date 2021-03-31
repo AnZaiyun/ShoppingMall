1e0d1d15715ce05b45067350832e58e780a9f188
AnZaiYun <1732478192@qq.com>
2021-04-01 00:15:08 +0800

    CompletableFuture测试

--- shoppingmall-search/src/main/java/com/anzaiyun/shoppingmall/search/thread/CompletableFutureTest.java
+++ shoppingmall-search/src/main/java/com/anzaiyun/shoppingmall/search/thread/CompletableFutureTest.java
@@ -30,25 +30,86 @@ public class CompletableFutureTest {
 //            return 10;
 //        });
 
-        CompletableFuture<Integer> supplyAsync = CompletableFuture.supplyAsync(() -> {
-            System.out.println("当前线程：" + Thread.currentThread().getId());
-            int i = 10 / 4;
-            System.out.println("运行结果：" + i);
-            return i;
-        }, executorService).handle((res,exc)->{
-            if(res!=null){
-                return res*3;
-            }
+//        CompletableFuture<Integer> supplyAsync = CompletableFuture.supplyAsync(() -> {
+//            System.out.println("当前线程：" + Thread.currentThread().getId());
+//            int i = 10 / 4;
+//            System.out.println("运行结果：" + i);
+//            return i;
+//        }, executorService).handle((res,exc)->{
+//            if(res!=null){
+//                return res*3;
+//            }
+//
+//            if (exc!=null){
+//                return 777;
+//            }
+//
+//            return 0;
+//        });
 
-            if (exc!=null){
-                return 777;
-            }
+//        CompletableFuture.supplyAsync(() -> {
+//            System.out.println("当前线程：" + Thread.currentThread().getId());
+//            int i = 10 / 4;
+//            System.out.println("运行结果：" + i);
+//            return i;
+//        }, executorService).thenAcceptAsync((result) -> {
+//
+//            System.out.println("任务2启动,获取到上一步返回值：" + result);
+//        }, executorService);
 
-            return 0;
-        });
+//        CompletableFuture<String> thenApplyAsync = CompletableFuture.supplyAsync(() -> {
+//            System.out.println("当前线程：" + Thread.currentThread().getId());
+//            int i = 10 / 4;
+//            System.out.println("运行结果：" + i);
+//            return i;
+//        }, executorService).thenApplyAsync((result) -> {
+//
+//            System.out.println("任务2启动,获取到上一步返回值：" + result);
+//            return "接收到返回值" + result;
+//        }, executorService);
 
-        Integer integer = supplyAsync.get();
-        System.out.println(integer);
+//        String s = thenApplyAsync.get();
+//        System.out.println(s);
+
+        CompletableFuture<String> future01 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("线程111");
+            return "线程111";
+        },executorService);
+
+        CompletableFuture<String> future02 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("线程222");
+            return "线程222";
+        }, executorService);
+
+//        future01.runAfterBothAsync(future02,()->{
+//            System.out.println("线程333");
+//        },executorService);
+
+//        future01.thenAcceptBothAsync(future02,(res01,res02)->{
+//            System.out.println("线程333...start...");
+//
+//            System.out.println("res01 is "+res01+",res02 is "+res02);
+//
+//            System.out.println("线程333...end...");
+//
+//        },executorService);
+
+        CompletableFuture<String> thenCombineAsync = future01.thenCombineAsync(future02, (res01, res02) -> {
+            System.out.println("线程333...start...");
+
+            System.out.println("res01 is " + res01 + ",res02 is " + res02);
+
+            System.out.println("线程333...end...");
+
+            return "线程333";
+
+        }, executorService);
+
+        System.out.println(thenCombineAsync.get());
+
+
+//        Integer integer = supplyAsync.get();
+//        System.out.println(integer);
 
         System.out.println("main end ...");
 

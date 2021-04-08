77d6b6235ca4597857d8baa5abe776f4b84f6174
AnZaiYun <1732478192@qq.com>
2021-04-08 19:52:03 +0800

    配置登录注册页面相互跳转功能

---
+++ shoppingmall-auth-server/src/main/java/com/anzaiyun/shoppingmall/authserver/config/WebControllerConfig.java
@@ -0,0 +1,17 @@
+package com.anzaiyun.shoppingmall.authserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 使用springMvc 自带的viewcontroller转发功能，直接配置页面跳转功能
+ */
+@Configuration
+public class WebControllerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/denglu").setViewName("login");
+    }
+}

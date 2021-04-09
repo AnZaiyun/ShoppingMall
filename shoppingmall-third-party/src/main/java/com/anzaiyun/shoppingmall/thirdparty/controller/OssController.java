3bac64a5c1712dc1f8d883a8c571e45e90eb2f3e
AnZaiyun <1732478192@qq.com>
2021-04-09 16:28:50 +0800

    第三方服务代码优化

--- shoppingmall-third-party/src/main/java/com/anzaiyun/shoppingmall/thirdparty/controller/OssController.java
+++ shoppingmall-third-party/src/main/java/com/anzaiyun/shoppingmall/thirdparty/controller/OssController.java
@@ -6,6 +6,7 @@ import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
 import com.anzaiyun.common.utils.R;
+import com.anzaiyun.shoppingmall.thirdparty.component.OssComponent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,73 +23,12 @@ import java.util.Map;
 public class OssController {
 
     @Autowired
-    OSS ossClient;
-
-    @Value("${spring.cloud.alicloud.oss.endpoint}")
-    private String endpoint;
-
-    private String accessId;
-    private String accessKey;
+    OssComponent ossComponent;
 
     @RequestMapping("/policy")
     public R policy(){
 
-        File csvFile = new File("C:\\Users\\AnZaiyun\\Desktop\\AccessKey.csv");
-        String[] key;
-        String inString = "";
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(csvFile));
-            while ((inString = reader.readLine())!=null){
-                key = inString.split(",");
-                accessId = key[0];
-                accessKey = key[1];
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String bucket = "an-shopping-mall"; // 请填写您的 bucketname 。
-        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
-        // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
-//        String callbackUrl = "http://88.88.88.88:8888";
-        String formatDir =  new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-        String dir = formatDir + "/"; // 用户上传文件时指定的前缀。
-
-        Map<String, String> respMap = null;
-
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessId, accessKey);
-        try {
-            long expireTime = 30;
-            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
-            Date expiration = new Date(expireEndTime);
-            // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。
-            PolicyConditions policyConds = new PolicyConditions();
-            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
-            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
-
-            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
-            byte[] binaryData = postPolicy.getBytes("utf-8");
-            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
-            String postSignature = ossClient.calculatePostSignature(postPolicy);
-
-            respMap = new LinkedHashMap<String, String>();
-            respMap.put("accessid", accessId);
-            respMap.put("policy", encodedPolicy);
-            respMap.put("signature", postSignature);
-            respMap.put("dir", dir);
-            respMap.put("host", host);
-            respMap.put("expire", String.valueOf(expireEndTime / 1000));
-            // respMap.put("expire", formatISO8601Date(expiration));
-
-        } catch (Exception e) {
-            // Assert.fail(e.getMessage());
-            System.out.println(e.getMessage());
-        } finally {
-            ossClient.shutdown();
-        }
+        Map<String, String> respMap = ossComponent.policy();
 
         return R.ok().put("data",respMap);
 
